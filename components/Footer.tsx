df5595690cd558d1968e1a74f9e42a99b9aea8ca
vamshichi <chvamshi03@gmail.com>
2026-08-12 13:15:26 +0530

    added space

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -105,7 +105,7 @@ export default function Footer() {
               href="mailto:theoakproject.gospelband@gmail.com"
               className="mt-5 block text-sm text-warm-gray transition-colors hover:text-gold"
             >
-              Kalebu C
+              Kalebu C<br></br>
               theoakproject.gospelband@gmail.com
             </a>
 
